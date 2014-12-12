4015130186e965ab3457cfb7cdd79b912f20e024
Kevin Pietrow <kpietrow@gmail.com>
2014-12-11 19:24:35 -0500

    this is currently set up as an eclipse java project, with ChessAI being the main folder

---
+++ src/Board.java
@@ -0,0 +1,10 @@
+/*
+ * So what I'm thinking for this is to have a board object that holds the 
+ * current state of the board. The only flaw with this method is that I did something 
+ * similar for my Rubik's cube project, and found that it was actually extremely difficult 
+ * to copy the board, without copying the reference as well. For example, if you 
+ * perform changes to a copy, those changes will be applied to the original as well.
+ */
+public class Board {
+
+}
