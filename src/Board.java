4e12a393035fbee63cd431cd268e1bd52019c3d7
thaiweis1 <thai.weis1@marist.edu>
2014-12-12 02:21:50 -0500

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -4,6 +4,11 @@
  * similar for my Rubik's cube project, and found that it was actually extremely difficult 
  * to copy the board, without copying the reference as well. For example, if you 
  * perform changes to a copy, those changes will be applied to the original as well.
+ *
+ *
+ * We could try making a 2-D array to represent the board. I'm not sure how that would play out,
+ * but it's simple if we just assign pieces to the board. We could do actual move calculation on the
+ * pieces themselves.
  */
 public class Board {
 
