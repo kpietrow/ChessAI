98f0bf18e48f74889adebd082ac720808b8253ae
Kevin Pietrow <kpietrow@gmail.com>
2014-12-12 19:05:46 -0500

    have successfully sent a move back to the server

--- src/Runnable.java
+++ src/Runnable.java
@@ -2,9 +2,11 @@ import java.io.BufferedReader;
 import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Scanner;
 
 import org.json.JSONException;
@@ -24,26 +26,46 @@ public class Runnable {
 	    String json = "";
 				
 		try {
-			// Queries the URL for the game
-			HttpURLConnection c = (HttpURLConnection) (new URL("http://www.bencarle.com/chess/poll/" + input + "/209/fcbd8a97/")).openConnection();
-			int resp = c.getResponseCode();
+			// URL to query the game
+			HttpURLConnection query = (HttpURLConnection) (new URL("http://www.bencarle.com/chess/poll/" + input + "/209/fcbd8a97/")).openConnection();
+			
 			
 			// Convert to BufferReader, grab the first line
-			BufferedReader reader = new BufferedReader(new InputStreamReader(c.getInputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(query.getInputStream()));
 			json = reader.readLine();
 			
 			// Going to attempt to make a json object out of this
 			try {
+				// It works! JSON object successfully created
 				JSONObject j = new JSONObject(json);
 				System.out.println("Here goes nothing!");
 				System.out.println(j.get("lastmovenumber"));
 				
-				// It works! JSON object successfully created
+				boolean d = (boolean) j.get("ready");
+				if (d) {
+					System.out.println("trueee");
+				} else {
+					System.out.println("falseee");
+				}
+				// And it has the correct value as well
 				
+				// Now to try to send something back
+				// URL to respond
+				
+				//This section works on sending a move!
+				HttpURLConnection response = (HttpURLConnection) (new URL("http://www.bencarle.com/chess/move/" + input + "/209/fcbd8a97/Pa2a3/")).openConnection();
+				BufferedReader reader2 = new BufferedReader(new InputStreamReader(response.getInputStream()));
+				System.out.println(reader2.readLine());
+				// Sends the move
+				
+				
+								
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
+			
 		    
 		  
 		} catch (MalformedURLException e) {
