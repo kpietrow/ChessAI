c8fea82fd280aa305c30df92d13696960dbfe994
Kevin Pietrow <kpietrow@gmail.com>
2014-12-14 15:33:12 -0500

    enhancing the evaluation function to include attack values for white pieces

--- src/ConstructWhiteMove.java
+++ src/ConstructWhiteMove.java
@@ -51,9 +51,107 @@ public class ConstructWhiteMove {
 				evalValue -= board.blackPieces[i].value;
 			}
 		}
-			
+		
+		// Now let's add the white side's attacks
+		for (int i = 0; i < board.whitePieces.length; i++) {
+			if (board.whitePieces[i].active) {
+				evalValue += calculateWhiteAttackValue(board.whitePieces[i], board);
+			}
+		}
+		
+		
+		
 		
 		return evalValue;
+	}
+	
+	// The hub for determining the potential value of a white piece's attacks
+	public int calculateWhiteAttackValue(Piece piece, Board board) {
+		
+		// Get the location of the piece on the board
+		int location = piece.location;
+		
+		int value = 0;
+		
+		// King
+		if (piece.pieceType == 6) {
+			value += calculateKingAttacks(location, board);
+		
+		// Queen
+		} else if (piece.pieceType == 5) {
+			value += calculateWhiteDiagonalAttacks(piece.location, board);
+			value += calculateWhiteRowAttacks(piece.location, board);
+		}
+		
+		
+		
+		return value;
+	}
+	
+	public int calculateKingAttacks(int location, Board board) {
+		int value = 0;
+		
+		// This means a black piece is there
+		if (board.state[location - 13] < 0) {
+			value += calculateAttackValue(board.blackPieces[board.state[location - 13]].pieceType);
+		} else if (board.state[location - 12] < 0) {
+			value += calculateAttackValue(board.blackPieces[board.state[location - 12]].pieceType);
+		} else if (board.state[location - 11] < 0) {
+			value += calculateAttackValue(board.blackPieces[board.state[location - 11]].pieceType);
+		} else if (board.state[location - 1] < 0) {
+			value += calculateAttackValue(board.blackPieces[board.state[location - 1]].pieceType);
+		} else if (board.state[location + 1] < 0) {
+			value += calculateAttackValue(board.blackPieces[board.state[location + 1]].pieceType);
+		} else if (board.state[location + 11] < 0) {
+			value += calculateAttackValue(board.blackPieces[board.state[location + 11]].pieceType);
+		} else if (board.state[location + 12] < 0) {
+			value += calculateAttackValue(board.blackPieces[board.state[location + 12]].pieceType);
+		} else if (board.state[location + 13] < 0) {
+			value += calculateAttackValue(board.blackPieces[board.state[location + 13]].pieceType);
+		}
+		
+		return value;
+	}
+	
+	public int calculateWhiteDiagonalAttacks(int location, Board board) {
+		
+		// Up and to the right 
+		// (Limit is 118, because 117 is the absolute highest that the board goes)
+		for (int i = location + 13; i < 118; i++) {
+			// The game is up!
+			if (board.state[i] == 99) {
+				break;
+			} else if (board.state[i] < 0) {
+				
+			}
+		}
+		
+		return 0;
+	}
+	
+	public int calculateWhiteRowAttacks(int location, Board board) {
+		
+		
+		return 0;
+	}
+	
+	
+	public int calculateAttackValue(int pieceType) {
+		if (pieceType == 1) {
+			return 1;
+		} else if (pieceType == 2) {
+			return 3;
+		} else if (pieceType == 3) {
+			return 5;
+		} else if (pieceType == 4) {
+			return 7;
+		} else if (pieceType == 5) {
+			return 10;
+		} else if (pieceType == 6) {
+			return 15;
+		}
+		
+		return 0;
 	}
 	
 }
