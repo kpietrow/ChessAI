9dce6a0e443ef1ccf23a8c72764ff7202e9686da
Kevin Pietrow <kpietrow@gmail.com>
2014-12-14 14:22:33 -0500

    Changed the construct move to white-specific. I figure that this will make it easier overall

--- src/ConstructWhiteMove.java
+++ src/ConstructWhiteMove.java
@@ -8,7 +8,7 @@ import java.net.URL;
 /*
  * This class will handle the construction of the next move
  */
-public class ConstructMove {
+public class ConstructWhiteMove {
 	
 	public static void run(String gameID, Board board) {
 		
@@ -34,27 +34,24 @@ public class ConstructMove {
 	 * This is going to be difficult. This is where the evaluation function will go.
 	 * 
 	 * Going to do a simple one to start off. Each piece will be added up. 
-	 * I am going to take into consideration white and black, with a true 'white'
-	 * parameter treated as the AI being white, and a false value means the AI is black
+	 * This class is dedicated to the white player, so only white's situation will
+	 * be considered.
 	 */
-	public int eval(boolean white, Board board) {
+	public int eval(Board board) {
 		
 		int evalValue = 0;
 		
-		if (white) {
-			// Adds values of white pieces, subtracts that of black
-			for (int i = 0; i < board.whitePieces.length; i++) {
+		// Adds values of white pieces, subtracts that of black
+		for (int i = 0; i < board.whitePieces.length; i++) {
+			if (board.whitePieces[i].active) {
 				evalValue += board.whitePieces[i].value;
-				evalValue -= board.blackPieces[i].value;
 			}
 			
-		// Adds values of black pieces, subtracts that of white	
-		} else {
-			for (int i = 0; i < board.whitePieces.length; i++) {
-				evalValue -= board.whitePieces[i].value;
-				evalValue += board.blackPieces[i].value;
+			if (board.blackPieces[i].active) {
+				evalValue -= board.blackPieces[i].value;
 			}
 		}
+			
 		
 		return evalValue;
 	}
